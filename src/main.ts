88d5ce36c3a513802f5588b4a576ea5d5cad4967
Bryan <bryanstevenal@gmail.com>
2023-01-26 20:05:10 +0100

    Import Icons

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,25 @@ import router from './router';
 
 import './assets/main.css';
 
+/*
+    ====================
+        FONTAWESOME
+    ====================
+*/
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* import specific icons */
+import { faChess } from '@fortawesome/free-solid-svg-icons';
+/* add icons to the library */
+library.add(faChess)
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon);    //  FONTAWESOME
 
 app.mount('#app');
