d6f2bb43615c60376cb507b9fa61bced8b821c66
Bryan <bryanstevenal@gmail.com>
2023-01-26 23:25:31 +0100

    Import letters and numbers icons

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,47 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 /* import specific icons */
-import { faChess } from '@fortawesome/free-solid-svg-icons';
+import {
+    faBorderTopLeft,
+    faChess,
+    fa1,
+    fa2,
+    fa3,
+    fa4,
+    fa5,
+    fa6,
+    fa7,
+    fa8,
+    faA,
+    faB,
+    faC,
+    faD,
+    faE,
+    faF,
+    faG,
+    faH,
+} from '@fortawesome/free-solid-svg-icons';
 /* add icons to the library */
-library.add(faChess);
+library.add(
+    faBorderTopLeft,
+    faChess,
+    fa1,
+    fa2,
+    fa3,
+    fa4,
+    fa5,
+    fa6,
+    fa7,
+    fa8,
+    faA,
+    faB,
+    faC,
+    faD,
+    faE,
+    faF,
+    faG,
+    faH
+);
 
 const app = createApp(App);
 
