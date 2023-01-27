c0bdd8565098f2fa0198ab3eca5a513920bfc245
Bryan <bryanstevenal@gmail.com>
2023-01-27 22:38:47 +0100

    Add aside to GameView

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import Board from '@/components/game/BoardComponent.vue';
+import Aside from '@/components/shared/AsideComponent.vue';
 </script>
 
 <template>
-    <article class="flex">
-        <section>
-            <Board />
-        </section>
-        <section>
-            <h2>GAME:</h2>
-        </section>
+    <article class="flex flex--column">
+        <article class="flex flex-4">
+            <section>
+                <Board />
+            </section>
+        </article>
+        <Aside />
     </article>
 </template>
 
