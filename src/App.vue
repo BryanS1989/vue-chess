ec6f455951247a433e3567ee02b0c884c56a6b8a
Bryan <bryanstevenal@gmail.com>
2023-01-27 21:54:51 +0100

    Remove footer component and create HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 
-import NavBar from '@/components/shared/NavbarComponent.vue';
-import Footer from '@/components/shared/FooterComponent.vue';
+import Header from '@/components/shared/HeaderComponent.vue';
 </script>
 
 <template>
-    <header>
-        <h1 class="flex">
-            <font-awesome-icon icon="fa-solid fa-chess" />
-            <p>VUE Chess</p>
-        </h1>
-        <NavBar />
-    </header>
+    <Header />
 
     <main>
         <RouterView />
     </main>
-
-    <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/shared/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import NavBar from '@/components/shared/NavbarComponent.vue';
+</script>
+
+<template>
+    <header>
+        <h1 class="flex flex--col">
+            <font-awesome-icon icon="fa-solid fa-chess" />
+            <p>VUE Chess</p>
+        </h1>
+        <NavBar />
+    </header>
+</template>
+
+<style scoped>
+header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    padding: 1rem;
+}
+</style>
