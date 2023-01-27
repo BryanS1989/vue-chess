e29eaa0be6d678f782085bc4a8dac3f374a2962a
Bryan <bryanstevenal@gmail.com>
2023-01-27 22:38:32 +0100

    Navbar 100% height

--- src/components/shared/NavbarComponent.vue
+++ src/components/shared/NavbarComponent.vue
@@ -3,10 +3,14 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <nav class="flex flex--col">
+    <nav class="flex flex--col justify--evenly">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/game">Game</RouterLink>
     </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+    height: 100%;
+}
+</style>
