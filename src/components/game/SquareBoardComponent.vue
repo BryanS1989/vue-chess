2c379bff7c77a9d90a9e57546637acc4b6855ac8
Bryan <bryanstevenal@gmail.com>
2023-01-27 23:22:15 +0100

    Create SquareBoard component

---
+++ src/components/game/SquareBoardComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    x: number;
+    y: number;
+}>();
+
+const cellColor = computed((): string => {
+    return (props.x + props.y) % 2 === 0 ? 'square--white' : 'square--black';
+});
+</script>
+
+<template>
+    <td
+        class="board_square"
+        :class="cellColor"
+    ></td>
+</template>
+
+<style scoped></style>
