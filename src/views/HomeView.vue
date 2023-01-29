909a1c7909fa04460d6e063e7c18961e9b2c3b61
Bryan <bryanstevenal@gmail.com>
2023-01-29 15:59:13 +0100

    Use PlayersForm at HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PlayersForm from '@/components/players/PlayersForm.vue';
+
+function startGame(start: boolean) {
+    console.log('[PlayersForm] [startNewGame()] Start game: ', start);
+}
+</script>
 
 <template>
-    <article></article>
+    <article>
+        <h2 class="text--center">WELCOME TO VUE-CHESS!!!</h2>
+        <PlayersForm @start-game="startGame" />
+    </article>
 </template>
